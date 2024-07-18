b7ba4742f36694c57c2ff1c57476841f09aba2b2
Rade <bjelicr@gmail.com>
2024-07-18 11:10:24 +0200

    AssemblyInfo

--- Restaurants.Domain/obj/Debug/net8.0/Restaurants.Domain.AssemblyInfo.cs
+++ Restaurants.Domain/obj/Debug/net8.0/Restaurants.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e797a2ca4eb5e73927185930e9e2823a9abbdf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20baf60aa97c5749355c30e63333a22d8153644")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
